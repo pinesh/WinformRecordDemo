64261fa6f96219674e635e382534070bf7e3a779
pinesh <harrypines@live.co.uk>
2022-04-06 09:49:09 -0700

    Base commit

---
+++ MoodMe NETDemo/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace MoodMe_NETDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TagTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TagTextBox
+            // 
+            this.TagTextBox.Location = new System.Drawing.Point(46, 236);
+            this.TagTextBox.Name = "TagTextBox";
+            this.TagTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TagTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TagTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TagTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MoodMe NETDemo/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoodMe_NETDemo
+{
+    public partial class Form1 : Form
+    {
+        private RecordingViewModel vm;
+        public Form1()
+        {
+            InitializeComponent();
+            vm = new RecordingViewModel();
+            //vm.RecordingBindingSource = 
+           // this.Load +=
+           //store paths of video
+        }
+        /*
+        <Grid Background = "White" HorizontalAlignment="Right" VerticalAlignment="Top"  >
+        <!-- overlay with hint text -->
+        <TextBlock Margin = "5,2" MinWidth="50" Text="Suche..." 
+        Foreground="LightSteelBlue" Visibility="{Binding ElementName=txtSearchBox, Path=Text.IsEmpty, Converter={StaticResource MyBoolToVisibilityConverter}}" IsHitTestVisible="False"/>
+        <!-- enter term here -->
+        <TextBox MinWidth = "50" Name="txtSearchBox" Background="Transparent" />
+        </Grid>
+        */
+        /// <summary>
+        /// Verify integrity of database.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+     
+    }
+
+    public class RecordingDataBase
+    {
+        private string connectionString;
+        private string dbname = "demo.db";
+        
+
+
+
+        public RecordingDataBase()
+        {
+            EstablishDB();
+        }
+
+
+        internal void Construct()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteCommand comm = new SQLiteCommand())
+                {
+                        comm.Connection = conn;
+                        comm.CommandText = " create table test (n integer) ";
+                        comm.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+        }
+
+        
+        internal void Transact(string video,string tag)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                using (SQLiteTransaction tran = conn.BeginTransaction())
+                {
+                    using (SQLiteCommand comm = new SQLiteCommand())
+                    {
+                        comm.Connection = conn;
+                        comm.CommandText = "INSERT INTO recordings(id,video,tag, creationDate) VALUES($id,$video,$tag)";
+                        comm.Parameters.AddWithValue("$id", DateTime.UtcNow.ToFileTime());
+                        comm.Parameters.AddWithValue("$video", video);
+                        comm.Parameters.AddWithValue("$tag", tag);
+                        comm.Prepare();
+                        comm.ExecuteNonQuery();
+                    }
+                    tran.Commit();
+                }
+                conn.Close();
+            }
+        }
+
+        internal void EstablishDB()
+        {
+            var dir = Application.ExecutablePath;
+            try
+            {
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                var dbPath = Path.Combine(dir,dbname);
+                connectionString = "Data Source=" + dbPath;
+                if (!File.Exists(dbPath))
+                {
+                    SQLiteConnection.CreateFile(dbPath);
+                    using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+                    {
+                        using (SQLiteCommand comm = new SQLiteCommand())
+                        {
+                            comm.Connection = conn;
+                            comm.CommandText = @"CREATE TABLE recordings(id INTEGER PRIMARY KEY,video TEXT, tag TEXT)";
+                            comm.ExecuteNonQuery();
+                        }
+                        conn.Close();
+                    }
+                }
+              
+            }
+            catch (System.Exception ex)
+            {
+                //MessageBox.Show(x.Message);
+            }
+        }
+    }
+
+    public class RecordingViewModel : IDisposable
+    {
+        private string db;
+
+
+        public RecordingViewModel() => db = "hehe";
+        public BindingSource RecordingBindingSource { get; set; }
+
+        public void Load()
+        {
+            //set recording binding source here
+
+        }
+
+        public void Remove() => RecordingBindingSource.RemoveCurrent();
+        public void New() => RecordingBindingSource.AddNew();
+
+        public void Save()
+        {
+            RecordingBindingSource.EndEdit();
+           
+            //save db changes and push them back
+        }
+        public void Dispose()
+        {
+            //dispose of db entity
+        }
+    }
+}
